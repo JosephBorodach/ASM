1274bc33e6b00e03d2bf4e7747604772bd5c8218
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2022-11-29 18:28:26 -0500

    uU

--- MandelbrotRenderer/MandelbrotRenderer 2/MBPixelCalc.s
+++ MandelbrotRenderer/MandelbrotRenderer 2/MBPixelCalc.s
@@ -1,25 +1,19 @@
 .data
 .text
-.globl _MBPixelCalc
-_MBPixelCalc:
-    shl $6, %rdx
-    sar $6, %rdx
-    shl $58, %rsi
-    sar $58, %rsi
-    shl $6, %rsi
-    sar $6, %rsi
-    shl $58, %rsi
-    sar $58, %rsi
+.globl MBPixelCalc
+MBPixelCalc:
     pushq %r8; pushq %r9
     pushq %r10; pushq %r11
-    pushq %r12; pushq %r13
-    pushq %r14; pushq %r15
-    pushq %rdx; pushq %rbx
-    movq $0, %r8; movq $0, %r9
-    movq $0, %r10; movq $0, %r11
+    pushq %r12
+    pushq %r13
+    pushq %r14
+    pushq %r15
+    pushq %rdx
+    movq $0, %r8
+    movq $0, %r9
+    movq $0, %r10
+    movq $0, %r11
     movq $0, %r12
-    movq $2, %rbx
-    imul $2, %rbx
 _loop:
     cmpq $1000, %r12
     jge _done
@@ -33,12 +27,12 @@ _loop:
     mov %r13, %rdx
     addq %r14, %rdx
 
-    cmpq %rbx, %rdx
+    cmpq $4, %rdx
     jge _done
 
     mov %r13, %r15
     subq %r14, %r15
-    addq %rcx, %r15
+    addq %rdi, %r15
 
     imul %r10, %r11
     imul $2, %r11
@@ -55,5 +49,5 @@ _done:
     movq %r12, %rax
     popq %r8; popq %r9; popq %r10; popq %r11
     popq %r12; popq %r13; popq %r14; popq %r15
-    popq %rdx; popq %rbx
+    popq %rdx
     ret
