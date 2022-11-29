c94f3e708559da969dbe04c9b3d0d7a246ccb078
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2022-11-29 11:46:55 -0500

    added

--- MandelbrotRenderer/test.s
+++ MandelbrotRenderer/test.s
@@ -2,6 +2,17 @@
 .text
 .globl test
 test:
+    xorq %r12, %r12
+    xorq %r13, %r13
+    xorq %r8, %r8
+    xorq %r9, %r9
+    xorq %rcx, %rcx
+    xorq %rcx, %rcx
+    xorq %rdp, %rdp
+    xorq %rsp, %rsp
+    xorq %r8, %r8
+    xorq %r9, %r9
+    xorq %rax, %rax
     xorq %rax, %rax
     movq $1, %rdx
     movq %rdi, %rax
