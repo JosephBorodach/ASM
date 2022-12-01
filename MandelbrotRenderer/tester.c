26ee3f0e6c3365535ac85b10eebe15728e37e79f
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2022-12-01 11:13:24 -0500

    a

--- MandelbrotRenderer/tester.c
+++ MandelbrotRenderer/tester.c
@@ -16,12 +16,12 @@ int main(int argc, char *argv[]) {
     long shift2 =  1;
     shift1 = shift1 << 58;
     shift2 = shift2 << 58;
-    long a = shift1 * x;
-    long b = shift2 * y;
-    //long a = 72057594037927936;
-    //long b = -322818021289917184;
-    printf("a: %ld\n", a);
-    printf("b: %ld\n", b);
+    //long a = shift1 * x;
+    //long b = shift2 * y;
+    long a = 72057594037927936;
+    long b = -322818021289917184;
+    //printf("a: %ld\n", a);
+    //printf("b: %ld\n", b);
     printf("MBPixelCalc() returned %ld.\n", MBPixelCalc(a, b));
     return 0;
 }
