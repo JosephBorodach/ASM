e243071aee1ba0b5f06e05d24f8a70f25d71889f
JosephBorodach <95253429+JosephBorodach@users.noreply.github.com>
2022-12-05 14:34:52 -0500

    u

--- MandelbrotRendererDoubles/MBPixelCalc.s
+++ MandelbrotRendererDoubles/MBPixelCalc.s
@@ -3,6 +3,34 @@
 .global MBPixelCalc
 MBPixelCalc:
     ###############################################
+    # %xmm1 == x : Originally, 2nd args / y0
+    # %xmm2 == y : Originally, 1st arg / x0
+    # %xmm3 == holder for x * x
+    # %xmm4 == holder for y * y
+    # %xmm5 == holder for (x * x) + (y * y)
+    #
+    # %xmm8 == (2*2) == 4
+    # %xmm9 == 1st arg / x0
+    # %xmm10 == 2nd args / y0
+    # %xmm11 == counter / number of iterations
+    #
+    # %rbx == counter / number of iterations
+    #
+    # %xmm6 ==
+    # %xmm7 ==
+    # %xmm12 ==
+    # %xmm13 ==
+    # %xmm14 ==
+    # %xmm15 ==
+    # %eax
+    # %exs
+    # %edx
+    # %rdi
+    # %rsi
+    # vcvttsd2si %xmm5, %rax
+    ###############################################
+
+    ###############################################
     # All registers a caller saved, so we don't need to deal with the stack!
     xorpd %xmm2, %xmm2; xorpd %xmm3, %xmm3; xorpd %xmm4, %xmm4; xorpd %xmm5, %xmm5
     xorpd %xmm6, %xmm6; xorpd %xmm7, %xmm7; xorpd %xmm8, %xmm8; xorpd %xmm9, %xmm9;
@@ -21,69 +49,20 @@ MBPixelCalc:
 
     ###############################################
     # Set xmm0 & xmm1 to 0, since they represent x & y
-    #xorpd %xmm0, %xmm0;
+    xorpd %xmm0, %xmm0;
     xorpd %xmm1, %xmm1;
-    ###############################################
-
-    ###############################################
-    # Set %xmm6 == 1000.0
-    # xor %rax, %rax
-    # mov $1000.0, %rax
-    # movd %rax, %xmm6
-    ###############################################
-
-    ###############################################
-    # Set %xmm7 == 1.0
-    # xor %rax, %rax
-    # mov $1.0, %rax
-    # movd %rax, %xmm7
     ###############################################
 
     ###############################################
     xor %rax, %rax
     mov $4, %rax
     vcvtsi2sd %rax, %xmm8, %xmm8
-    ###############################################
-
-    ###############################################
     xor %rax, %rax
     ###############################################
-
 _loop:
-    ###############################################
-    # %xmm1 == x : Originally, 2nd args / y0
-    # %xmm2 == y : Originally, 1st arg / x0
-    # %xmm3 == holder for x * x
-    # %xmm4 == holder for y * y
-    # %xmm5 == holder for (x * x) + (y * y)
-    #
-    # Might not need!
-    # %xmm6 == 1000
-    # %xmm7 == Incremeting 1
-    # %xmm8 == 4, aka (2*2)
-    #
-    #
-    # %xmm9 == 1st arg / x0
-    # %xmm10 == 2nd args / y0
-    # %xmm11 == counter / number of iterations
-    #
-    # %rbx == counter / number of iterations
-    #
-    # %xmm12 ==
-    # %xmm13 ==
-    # %xmm14 ==
-    # %xmm15 ==
-    # %eax
-    # %exs
-    # %edx
-    # %rdi
-    # %rsi
-    ###############################################
-
     ###############################################
     # counter <= 1000.0
     # xmm6 == 1000.0
-    # ucomisd %xmm6, %xmm11
     cmpq $1000, %rbx
     jge _done
     ###############################################
@@ -101,47 +80,9 @@ _loop:
     ###############################################
 
     ###############################################
-    # Do the %rdx and %rax magic
-    # place %rax into %rdx if rdx is zero
-    # If %rdx is not zero, and this fails, then it will jump to con
-    # cmpq $0, %rdx
-    # jne .con
-    # movq %rax, %rdx      # Place %rax into %rdx
-
-    # Move (x * x) into %xmm3 - the temporary holder
-    # Bit shift 6 to get the correct number
-    # .con:
-    # Do we still need to bit shift?
-    # salq $6, %rdx
-    # movq %rdx, %xmm3
-    ###############################################
-
-    ###############################################
-    # Keep them fresh and set to zero
-    # movq $0, %rax
-    # movq $0, %rdx
-    ###############################################
-
-    ###############################################
     # (y * y): Move y (%xmm1) into %xmm4 and square %xmm4
     movq %xmm1, %xmm4
     mulsd %xmm4, %xmm4
-    ###############################################
-
-    ###############################################
-    # Do the %rdx and %rax magic
-    # place %rax into %rdx if rdx is zero
-    # If %rdx is not zero, and this fails, then it will jump to conn
-    # cmpq $0, %rdx
-    # jne .conn
-    # movq %rax, %rdx      # Place %rax into %rdx
-
-    # Bit shift 6 to get the correct number
-    # Move (y * y) into %xmm4 - the temporary holder
-    # .conn:
-    # Do we still need to bit shift?
-    # salq $6, %rdx
-    # movq %rdx, %xmm4
     ###############################################
 
     ###############################################
@@ -157,7 +98,7 @@ _loop:
     # %xmm5 == (x*x + y*y)
     # %xmm8 == (2*2)
     ucomisd %xmm8, %xmm5
-    jge _done
+    jae _done
     ###############################################
 
     ###############################################
@@ -198,7 +139,6 @@ _loop:
 
     ###############################################
     # Increment the counter
-    # addpd %xmm7, %xmm11
     add $1, %rbx
     ###############################################
 
@@ -211,8 +151,6 @@ _done:
     # Since we are returning an int, the value must be return in %xmm0
     # Move the number of iterations into %xmm11
     # set %rax to 0
-    # Then set it to xmm11
     xor %rax, %rax
     movq %rbx, %rax
-    # movq %xmm11, %rax
     ret
